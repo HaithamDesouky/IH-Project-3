800cbc675f1546fa6d5795128caf85d24144c09b
João Maria <joao.ftf.maria@gmail.com>
2020-08-24 17:31:39 +0100

    merging from haitham

--- models/credits.js
+++ models/credits.js
@@ -6,17 +6,8 @@ const schema = new mongoose.Schema({
   name: String,
   picture: String,
   price: {
-<<<<<<< HEAD
     amount: Number,
-    currency: {
-      type: String,
-      enum: ["EUR"],
-    },
   },
-=======
-    amount: Number
-  }
->>>>>>> d16ce218b0dea3354f9042a892742b01b4a489f7
 });
 
 module.exports = mongoose.model("Credits", schema);
